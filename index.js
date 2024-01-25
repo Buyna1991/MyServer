221d956932c8ec42ebd9b832cc811c638282bc55
Buyanbat Batsukh <buyna@Buyanbats-MacBook-Pro-2.local>
2024-01-25 12:26:46 -0600

    update

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const port = 8000;
 const myServer = express();
+const fs=require("fs");
 
-myServer.get('/', (request, response) => {
-    response.send("Hello Buyna watch out bro");
-})
+const user = require("./users.json");
+myServer.use(express.json());
 
-myServer.listen(port,()=> {
-    console.log("My server running");
-}); 
+myServer.get("/users", (request, response) => {
+  response.json(user);
+});
+myServer.get("/users/:id", (request, response) => {
+  const userId = request.params.id;
+  const found = user.find((user) => user.id == userId);
+  if (found) {
+    response.json(found);
+  } else {
+    response.send("User not found");
+  }
+});
+myServer.post("/users/create", (request, response) => {
+  const newUser = request.body;
+  const { name } = newUser;
+  console.log(newUser);
+  user.push({ id: String (user.length + 1), name: name });
+  fs.writeFileSync("./users.json",JSON.stringify(user));
+  response.send(user);
+});
+
+myServer.listen(port, () => {
+  console.log("My server running");
+});
